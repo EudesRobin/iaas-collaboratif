d0827f73da32b40f15bff307af2e5a72d4abca32
AlanDamotte <Alan.damotte@gmail.com>
2016-02-03 13:56:01 +0100

    [ADA] gitignore + delete temp files + docker_shinken source

--- docker/start.sh
+++ docker/start.sh
@@ -3,21 +3,14 @@
 # Wrong parameters and empty parameters? => Need to be handled
 
 #========================================================
-# Parameters:
-# 1. Image type
-# 2. Number of containers to be launched
-# 3. Container name
-# 4. Available memory
-# 5. Unit
-
 if [[ "${1}" == "help" ]];then
 	echo Parameters:
 	echo 1. Image type
 	echo 2. Number of containers to be launched
 	echo 3. Container name
-	echo 4. Memory of 1 container
-	echo 5. Memory unit: M,G,T 
-	echo 6. Number of CPU going to be used by 1 container
+	echo 4. Memory of 1 container \(don\'t forget unit\)
+	echo 5. Number of CPU going to be used by 1 container
+	echo 6. Memory available set by the collaborator \(don\'t forget unit\)
 	exit 0
 fi
 #========================================================
@@ -35,7 +28,9 @@ fi
 
 #========================================================
 # Allocating memory
-allocatedMemory=${4}${5}
+memoryHardLimit=${6}
+memorySoftLimit=${4}
+
 #========================================================
 
 coreNumber=$(cat /proc/cpuinfo | grep 'core id' | uniq | wc -l)
@@ -61,18 +56,22 @@ do
 			#============================================
 			# Allocating CPU share
 			nbCPUMax=$((($j * $hyperthreadValue)-1))
-			nbCPUMin=$((($j * $hyperthreadValue)-(${6} * $hyperthreadValue)))
+			nbCPUMin=$((($j * $hyperthreadValue)-(${5} * $hyperthreadValue)))
 			cpuset="$nbCPUMin-$nbCPUMax"
 
 			#============================================
+			# Running docker containers
 			imgType="${3}$i"
 			echo "Running ${imgType} with "$allocatedMemory" of memory" 
-			docker run -ti -d -m $allocatedMemory --cpuset-cpus="$cpuset" --name $imgType --link vm:vm$imgType ${1}
-			for ((l=0; l<${6}; l++));
+			docker run -ti -d -m $memoryHardLimit --memory-reservation $memorySoftLimit --cpuset-cpus="$cpuset" --name $imgType --link vm:vm$imgType ${1}
+			for ((l=0; l<${5}; l++));
 			do
+				#========================================
+				# Updating CPU usage in coreFile.data
 				id=$(( $j - $l ))
 				cpu="cpu$id"
 				sed -i 's/\('${cpu}',\).*/\11,'${imgType}'/' ./coreFile.data
+				#========================================
 			done
 			
 			break
@@ -81,11 +80,4 @@ do
 done
 #========================================================
 
-#docker exec -ti $imgType bin/bash
-
 exit 0
-
-
-#nbCPUMax=$((($j * $hyperthreadValue)-1-(${7} * $hyperthreadValue)-($i * $hyperthreadValue)))
-#nbCPUMin=$((($j * $hyperthreadValue)-($i * $hyperthreadValue)-(${7} * $hyperthreadValue)-(${6} * $hyperthreadValue)))
-#cpuset="$nbCPUMin-$nbCPUMax"
