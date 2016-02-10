d2a47d12610e44c1c4b2f45a2eedab1d788df866
AlanDamotte <Alan.damotte@gmail.com>
2016-02-10 16:34:49 +0100

    [ADA] Update Dockerfiles

--- docker/launchCollaborator.sh
+++ docker/launchCollaborator.sh
@@ -6,15 +6,13 @@
 #====================================
 
 #====================================
-# Build coordinator and shinken images and launch associated containers
-./coordinatorStart.sh
+# Copy public key to Dockerfile folder
+cp ./frontend.pub ./vm/frontend.pub
 #====================================
 
 #====================================
-# Copy public key to Dockerfile folder
-cp ./frontend.pub ./images/centos/frontend.pub
-cp ./frontend.pub ./images/ubuntu/frontend.pub
-cp ./frontend.pub ./images/debian/frontend.pub
+# Build coordinator and shinken images and launch associated containers
+./coordinatorStart.sh
 #====================================
 
 #====================================
