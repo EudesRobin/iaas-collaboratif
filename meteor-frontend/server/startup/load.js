1b629a44e209821ae2344db054a0716c6ecb4e41
EudesRobin <eudes.robin@gmail.com>
2016-02-12 12:06:05 +0100

    [REU] fix cmd

--- meteor-frontend/server/startup/load.js
+++ meteor-frontend/server/startup/load.js
@@ -6,7 +6,7 @@ Meteor.startup(function () {
   var Future = Npm.require("fibers/future");
   // Load exec
   var exec = Npm.require("child_process").exec;
- 
+  
   // Server methods
   Meteor.methods({
     exec_cmd: function (cmd, params) {
@@ -17,15 +17,15 @@ Meteor.startup(function () {
       var future=new Future();
 
       var command;
-     switch (cmd) {
+      switch (cmd) {
         case "stop":
-         command="ssh nodetest@nodetest 'docker exec  coordinator ssh iaas@172.17.0.1 stop.sh "+params+"'";
-          break;
+        command="ssh nodetest@nodetest 'docker exec  coordinator ssh iaas@172.17.0.1 /home/iaas/stop.sh "+params+"'";
+        break;
         case "create":
-         command="ssh nodetest@nodetest 'docker exec  coordinator ssh iaas@172.17.0.1 start.sh "+params+"'";
-          break;
+        command="ssh nodetest@nodetest 'docker exec  coordinator ssh iaas@172.17.0.1 /home/iaas/start.sh "+params+"'";
+        break;
         default:
-          return;
+        return;
       }
 
       exec(command,function(error,stdout,stderr){
