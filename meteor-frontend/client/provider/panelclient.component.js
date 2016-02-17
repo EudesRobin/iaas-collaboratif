76665f6e34564d0138742a72d47e962a00b801bf
Romain Barthelemy <romain.badamo-barthelemy@e.ujf-grenoble.fr>
2016-02-17 15:51:06 +0100

    [RBA] Commented a part of stopRessource that should update the Machines table

--- meteor-frontend/client/provider/panelclient.component.js
+++ meteor-frontend/client/provider/panelclient.component.js
@@ -51,6 +51,11 @@ angular.module('iaas-collaboratif').directive('provider', function () {
 
 			this.stopRessource = (ressource) => {
 				ressource.usable=false;
+				// for(int i=0;i<ressource.machines_id.length;i++){
+				// 	Machines.update({_id: ressource.machines_id[i]}, {$set:{state:'down'}}, (error) => {
+				// 	if (error) console.error('Oops, unable to update the machine...');
+				// 	else console.log('Done!');
+				// }
 				Ressources.update({_id: ressource._id}, {$set:{usable:ressource.usable}}, (error) => {
 					if (error) console.error('Oops, unable to update the ressource...');
 					else console.log('Done!');
