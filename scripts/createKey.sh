faf276cdf0a73555027b57ad759bacd313050ed8
Romain Barthelemy <romain.badamo-barthelemy@e.ujf-grenoble.fr>
2016-02-18 14:22:30 +0100

    [RBA] Script to put SSH key in a file done

--- scripts/createKey.sh
+++ scripts/createKey.sh
@@ -3,4 +3,4 @@
 # Author : Romain Barthelemy
 cd ../meteor-frontend/
 
-echo "db.users.findOne({_id : "\""$1"\""}).subscriber.sshKey;" | meteor mongo >> ../scripts/$1.pub
+echo "db.users.findOne({_id : "\""$1"\""}).subscriber.sshKey;" | meteor mongo | awk 'NR==3' >> ../scripts/$1.pub
