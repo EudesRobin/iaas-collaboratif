9586498791361d6012af1fad7086abd72cd12dae
EudesRobin <eudes.robin@gmail.com>
2016-02-20 19:32:17 +0100

    [REU] start script fixed

--- scripts/start-meteor-frontend.sh
+++ scripts/start-meteor-frontend.sh
@@ -2,12 +2,12 @@
 
 # Author : Robin Eudes
 
-# Use : start Meteor , for our app 
-#if ! test "${HOME}/iaas-collaboratif"
-#then
-#	echo "folder ${HOME}/iaas-collaboratif not found"
-#	exit 1
-#else
+
+if [ ! -d "${HOME}/iaas-collaboratif" ];
+then
+	echo "folder ${HOME}/iaas-collaboratif not found"
+	exit 1;
+else
 	${HOME}/iaas-collaboratif/scripts/check_running.sh
-	echo "Meteor started..."
-#fi
+	echo "Meteor started...";
+fi
