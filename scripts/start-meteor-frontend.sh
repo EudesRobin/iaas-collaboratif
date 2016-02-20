b039cac7072c863783e0778487d2076cd2e8ecfe
EudesRobin <eudes.robin@gmail.com>
2016-02-20 17:46:49 +0100

    [REU] data send to modal provider - update fct TODO

--- scripts/start-meteor-frontend.sh
+++ scripts/start-meteor-frontend.sh
@@ -3,11 +3,11 @@
 # Author : Robin Eudes
 
 # Use : start Meteor , for our app 
-if ! test "${HOME}/iaas-collaboratif"
-then
-	echo "folder ${HOME}/iaas-collaboratif not found"
-	exit 1
-else
+#if ! test "${HOME}/iaas-collaboratif"
+#then
+#	echo "folder ${HOME}/iaas-collaboratif not found"
+#	exit 1
+#else
 	${HOME}/iaas-collaboratif/scripts/check_running.sh
 	echo "Meteor started..."
-fi
+#fi
