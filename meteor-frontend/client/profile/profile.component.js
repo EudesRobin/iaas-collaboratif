2d4a09cb16ad1ffed21f170c4a515ad9c3dd1e33
EudesRobin <eudes.robin@gmail.com>
2016-02-22 22:46:40 +0100

    [REU] update username

--- meteor-frontend/client/profile/profile.component.js
+++ meteor-frontend/client/profile/profile.component.js
@@ -13,19 +13,76 @@ angular.module('iaas-collaboratif').directive('profile', function () {
 					return Meteor.users.findOne(Meteor.userId());
 				}
 			});
+			this.throw_error = (cmd,params) => {
+				var title;
+				switch(cmd){
+					case "update":
+					title = "Error update profile"
+					break;					
+					default:
+					title = "Error Unknown command"
+				}
+				$.notify({
+										// options
+										icon: 'glyphicon glyphicon-remove-sign',
+										title: title+"<br>",
+										message: params,
+									},{
+										//settings
+										type: 'danger',
+										newest_on_top: true,
+										allow_dismiss: true,
+										template: '<div data-notify="container" class="col-xs-6 col-sm-3 alert alert-{0}" role="alert">' +
+										'<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+										'<span data-notify="icon"></span> ' +
+										'<span data-notify="title">{1}</span> ' +
+										'<span data-notify="message">{2}</span>' +
+										'</div>' ,
+									});
+			};
+
+			this.throw_success = (cmd,params) => {
+				var title;
+				var message = "successful"
+				switch(cmd){
+					case "update":
+					title = "Update profile"
+					break;					
+					default:
+					title = "Unknown command"
+				}
+				$.notify({
+										// options
+										icon: 'glyphicon glyphicon-ok-sign',
+										title: title+"<br>",
+										message: message,
+									},{
+										//settings
+										type: 'success',
+										newest_on_top: true,
+										allow_dismiss: true,
+										template: '<div data-notify="container" class="col-xs-6 col-sm-3 alert alert-{0}" role="alert">' +
+										'<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+										'<span data-notify="icon"></span> ' +
+										'<span data-notify="title">{1}</span> ' +
+										'<span data-notify="message">{2}</span>' +
+										'</div>' ,
+									});
+			};
 
 			this.save = () => {
-
+				console.log(this.currentUser.username);
 				Meteor.users.update({_id: Meteor.userId()}, {
 					$set: {
-						emails: [this.currentUser.emails[0]]
+						emails: [this.currentUser.emails[0]],
+						username: this.currentUser.username
 					}
 				}, (error) => {
 					if (error) {
-						console.log('Oops, unable to update the user...');
+						this.throw_error('update','Failure during update');
 					}
 					else {
-						console.log('Done!');
+						this.throw_success('update','OK');
 					}
 				});
 			};
