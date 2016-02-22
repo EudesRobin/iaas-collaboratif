b2ab357a5cdc9df735d61653af88b4755604461e
Malek Mammar <malek.mammar@voxity.fr>
2016-02-22 13:28:49 +0100

    adding rabbitmq ssl scripts for creating a CA + coord. certificates

--- meteor-frontend/server/rabbitmq.js
+++ meteor-frontend/server/rabbitmq.js
@@ -5,18 +5,20 @@ Meteor.startup(function () {
 	var url = "amqp://guest:guest@localhost:5672";
 	var queue = "coordinators";
 	amqp.connect(url, function(err, conn) {
-	    if (err != null) bail(err);
+	    if (err != null) return console.error("AMQP - Failed to create a connection", err);
 		conn.createChannel(function (err, ch) {
-		    if (err) return console.error("AMQP - Failed to create a channel");
+		    if (err) return console.error("AMQP - Failed to create a channel", err);
 		    channel = ch;
 
 		    ch.assertQueue(queue, {}, function(err, ok){
-		    	if (err) return console.error("AMQP - Failed to create a queue")
+		    	if (err) return console.error("AMQP - Failed to create a queue", err)
 
 			    ch.consume(queue, function(msg) {
 			      	if (msg) 
 			      	{
-			      		data = JSON.parse(msg.content.toString());
+			      		// data = JSON.parse(msg.content.toString());
+			      		data = (msg.content.toString());
+    					console.log(msg);
 			        	doWork(data)
 			        	ch.ack(msg);
 			      	}
