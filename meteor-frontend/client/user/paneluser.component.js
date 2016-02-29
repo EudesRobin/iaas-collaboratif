4a920add27a23f6634c6b4b5f560ad9e0ceb487a
EudesRobin <eudes.robin@gmail.com>
2016-02-29 13:33:17 +0100

    [REU] Generate all-in-one ssh config file

--- meteor-frontend/client/user/paneluser.component.js
+++ meteor-frontend/client/user/paneluser.component.js
@@ -140,90 +140,127 @@ angular.module('iaas-collaboratif').directive('user', function () {
 				});
 			};
 
-			this.throw_error = (cmd,params) => {
-				var title;
-				switch(cmd){
-					case "create":
-					title = "Error creation instance"
-					break;
-					case "stop":
-					title = "Error kill instance"
-					break;
-					case "remove":
-					title = "Error remove instance"
-					break;						
-					default:
-					title = "Error Unknown command"
-				}
-				$.notify({
-										// options
-										icon: 'glyphicon glyphicon-remove-sign',
-										title: title+"<br>",
-										message: params,
-									},{
-										//settings
-										type: 'danger',
-										newest_on_top: true,
-										allow_dismiss: true,
-										template: '<div data-notify="container" class="col-xs-6 col-sm-3 alert alert-{0}" role="alert">' +
-										'<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
-										'<span data-notify="icon"></span> ' +
-										'<span data-notify="title">{1}</span> ' +
-										'<span data-notify="message">{2}</span>' +
-										'</div>' ,
-									});
-			};
+		this.throw_error = (cmd,params) => {
+			var title;
+			switch(cmd){
+				case "create":
+				title = "Error creation instance"
+				break;
+				case "stop":
+				title = "Error kill instance"
+				break;
+				case "remove":
+				title = "Error remove instance"
+				break;						
+				default:
+				title = "Error Unknown command"
+			}
+			$.notify({
+												// options
+												icon: 'glyphicon glyphicon-remove-sign',
+												title: title+"<br>",
+												message: params,
+											},{
+												//settings
+												type: 'danger',
+												newest_on_top: true,
+												allow_dismiss: true,
+												template: '<div data-notify="container" class="col-xs-6 col-sm-3 alert alert-{0}" role="alert">' +
+												'<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+												'<span data-notify="icon"></span> ' +
+												'<span data-notify="title">{1}</span> ' +
+												'<span data-notify="message">{2}</span>' +
+												'</div>' ,
+											});
+		};
 
-			this.startMachine = (machine,params) => {
-				this.save();
-				temp_machine = Ressources.find({_id: machine.ressource_id}).fetch();
+		this.startMachine = (machine,params) => {
+			this.save();
+			temp_machine = Ressources.find({_id: machine.ressource_id}).fetch();
 
-				if (temp_machine[0].usable){
-					machine.state='up';
-					Machines.update({_id: machine._id}, {$set:{state:machine.state}}, (error) => {
-						if (error) this.throw_error('create','Unable to start machine');
-						else this.action_user('create',machine.dns+'-'+params);
-					});
-				}
-			};
-
-			this.stopMachine = (machine) => {
-				machine.state='down';
+			if (temp_machine[0].usable){
+				machine.state='up';
 				Machines.update({_id: machine._id}, {$set:{state:machine.state}}, (error) => {
-					if (error) this.throw_error('stop','Unable to stop machine');
-					else this.action_user('stop',machine);
+					if (error) this.throw_error('create','Unable to start machine');
+					else this.action_user('create',machine.dns+'-'+params);
 				});
+			}
+		};
+
+		this.stopMachine = (machine) => {
+			machine.state='down';
+			Machines.update({_id: machine._id}, {$set:{state:machine.state}}, (error) => {
+				if (error) this.throw_error('stop','Unable to stop machine');
+				else this.action_user('stop',machine);
+			});
+		};
+
+		this.deleteMachine = (machine) => {
+			this.save();
+			this.currentUser.getSubscriber().desallocate(machine);
+			// notif done in subscriber.js
+		};		
+
+		this.generate = (machine) => {
+
+			makeTextFile = function (text) {
+				var data = new Blob([text], {type: 'text/plain'});
+				if (textFile !== null) {
+					window.URL.revokeObjectURL(textFile);
+				}
+				var textFile = window.URL.createObjectURL(data);
+				return textFile;
 			};
 
-			this.deleteMachine = (machine) => {
-				this.save();
-				this.currentUser.getSubscriber().desallocate(machine);
-							// notif done in subscriber.js
-			};		
+			function downloadURI(uri, name) {
+				var link = document.createElement("a");
+				link.download = name;
+				link.href = uri;
+				link.click();
+			}
 
-			this.generate = (machine) => {
+			var ssh_string='# Host is an alias , Hostname is the name of the user instance\n';
+			ssh_string+='# We presume that you private key is  ~/.ssh/client_pk\n';
+			ssh_string+='# To ssh your instance : type the following command\n';
+			ssh_string+='# ssh -F iaas-'+machine.machinename+'.config '+machine.machinename+'\n';
+			ssh_string+='Host '+machine.machinename+'\n';
+			ssh_string+='\tHostname '+machine.machinename+'\n';
+			ssh_string+='\tStrictHostKeyChecking no\n';
+			ssh_string+='\tProxyCommand  ssh -o "StrictHostKeyChecking no" -i "~/.ssh/client_pk" iaas-client@'+machine.dns+' netcat -w 120 %h %p\n';
+			ssh_string+='\tUser iaas-client\n';
+			ssh_string+='\tStrictHostKeyChecking no\n';
+			ssh_string+='\tIdentityFile ~/.ssh/client_pk';
 
-				makeTextFile = function (text) {
-					var data = new Blob([text], {type: 'text/plain'});
-					if (textFile !== null) {
-						window.URL.revokeObjectURL(textFile);
-					}
-					var textFile = window.URL.createObjectURL(data);
-					return textFile;
-				};
-				var id = machine._id;
-				var link = document.getElementById(id);
+			downloadURI(makeTextFile(ssh_string),'iaas-'+machine.machinename+'.config');
+		};
 
-				function downloadURI(uri, name) {
-					var link = document.createElement("a");
-					link.download = name;
-					link.href = uri;
-					link.click();
+		this.generate_all = () => {
+
+			makeTextFile = function (text) {
+				var data = new Blob([text], {type: 'text/plain'});
+				if (textFile !== null) {
+					window.URL.revokeObjectURL(textFile);
 				}
-				var ssh_string='# Host is an alias , Hostname is the name of the user instance\n';
-				ssh_string+='# We presume that you private key is  ~/.ssh/client_pk\n';
-				ssh_string+='# To ssh your instance : type the following command\n';
-				ssh_string+='# ssh -F iaas-'+machine.machinename+'.config '+machine.machinename+'\n';
+				var textFile = window.URL.createObjectURL(data);
+				return textFile;
+			};
+
+			function downloadURI(uri, name) {
+				var link = document.createElement("a");
+				link.download = name;
+				link.href = uri;
+				link.click();
+			}
+
+			var ssh_string="";
+			ssh_string+='# Host is an alias , Hostname is the name of the user instance\n';
+			ssh_string+='# We presume that you private key is  ~/.ssh/client_pk\n';
+			ssh_string+='# To ssh your instance :\n';
+			ssh_string+='# ssh -F <config_file> <instance_name>\n';
+
+			for(i=0;i<this.machines.length;i++){
+				var machine = this.machines[i];
+				ssh_string+='#Instance '+machine.machinename+'\n';
 				ssh_string+='Host '+machine.machinename+'\n';
 				ssh_string+='\tHostname '+machine.machinename+'\n';
 				ssh_string+='\tStrictHostKeyChecking no\n';
@@ -231,10 +268,10 @@ angular.module('iaas-collaboratif').directive('user', function () {
 				ssh_string+='\tUser iaas-client\n';
 				ssh_string+='\tStrictHostKeyChecking no\n';
 				ssh_string+='\tIdentityFile ~/.ssh/client_pk';
+			}
 
-				downloadURI(makeTextFile(ssh_string),'iaas-'+machine.machinename+'.config');
-			};
-
-		}
+			downloadURI(makeTextFile(ssh_string),'iaas-'+this.currentUser.username+'.config');
+		};
 	}
+}
 });
