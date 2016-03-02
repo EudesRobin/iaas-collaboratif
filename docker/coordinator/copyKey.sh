e43c053109bb924ef01eec139e1120c801abb744
AlanDamotte <Alan.damotte@gmail.com>
2016-03-02 15:47:43 +0100

    [ADA] Fix key's pb on coordinator

--- docker/coordinator/copyKey.sh
+++ docker/coordinator/copyKey.sh
@@ -4,7 +4,7 @@
 echo "Copying ssh coordinator public key"
 echo "=================================="
 echo
-sshpass -p iaas ssh-copy-id -o "StrictHostKeyChecking no" -p22 iaas@172.17.0.1
+sshpass -p iaas ssh-copy-id -i home/iaas-admin/.ssh/id_rsa.pub -o "StrictHostKeyChecking no" -p22 iaas@172.17.0.1
 sshpass -p iaas ssh-copy-id -o "StrictHostKeyChecking no" -p22000 iaas@172.17.0.1
 echo
 echo "=================================="
