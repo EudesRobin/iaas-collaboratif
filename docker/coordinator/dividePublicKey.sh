324706f0684e69ca4f0343aefe233f35126ce6f7
EudesRobin <eudes.robin@gmail.com>
2016-03-03 20:34:56 +0100

    [REU] fix dividekey

--- docker/coordinator/dividePublicKey.sh
+++ docker/coordinator/dividePublicKey.sh
@@ -12,7 +12,7 @@ fi
 #====================================
 # Copy client public key as "authorized_keys" in the iaas user home
 # This file will be automatically copied in every new container run
-scp -P 22000 -o "StrictHostKeyChecking no" -o "BatchMode=yes" -o "ConnectTimeout=5"  -o "UserKnownHostsFile=/dev/null" -o "GlobalKnownHostsFile=/dev/null" iaas@172.17.0.1:./authorized_keys &>/dev/null
+scp -P 22000 -o "StrictHostKeyChecking no" -o "BatchMode=yes" -o "ConnectTimeout=5"  -o "UserKnownHostsFile=/dev/null" -o "GlobalKnownHostsFile=/dev/null" ${1} iaas@172.17.0.1:./authorized_keys &>/dev/null
 #====================================
 
 
