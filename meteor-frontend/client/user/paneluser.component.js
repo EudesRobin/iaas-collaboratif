50f0c1fce2a35274b4eb4412d0ac8a10433cdddd
EudesRobin <eudes.robin@gmail.com>
2016-03-04 12:20:12 +0100

    [REU] remove call stop first now

--- meteor-frontend/client/user/paneluser.component.js
+++ meteor-frontend/client/user/paneluser.component.js
@@ -81,21 +81,21 @@ angular.module('iaas-collaboratif').directive('user', function () {
 							title = "Error unknown command"
 						}
 						$.notify({
-							// options
-							icon: 'glyphicon glyphicon-remove-sign',
-							title: title+"<br>",
-							message: err.details,
+						// options
+						icon: 'glyphicon glyphicon-remove-sign',
+						title: title+"<br>",
+						message: err.details,
 						},{
-							//settings
-							type: 'danger',
-							newest_on_top: true,
-							allow_dismiss: true,
-							template: '<div data-notify="container" class="col-xs-6 col-sm-3 alert alert-{0}" role="alert">' +
-							'<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
-							'<span data-notify="icon"></span> ' +
-							'<span data-notify="title">{1}</span> ' +
-							'<span data-notify="message">{2}</span>' +
-							'</div>' ,
+						//settings
+						type: 'danger',
+						newest_on_top: true,
+						allow_dismiss: true,
+						template: '<div data-notify="container" class="col-xs-6 col-sm-3 alert alert-{0}" role="alert">' +
+						'<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+						'<span data-notify="icon"></span> ' +
+						'<span data-notify="title">{1}</span> ' +
+						'<span data-notify="message">{2}</span>' +
+						'</div>' ,
 						});
 					}
 
@@ -116,65 +116,65 @@ angular.module('iaas-collaboratif').directive('user', function () {
 							title = "Unknown command"
 						}
 						$.notify({
-							// options
-							icon: 'glyphicon glyphicon-ok-sign',
-							title: title,
-							message: msg,
+						// options
+						icon: 'glyphicon glyphicon-ok-sign',
+						title: title,
+						message: msg,
 						},{
-							//settings
-							type: 'success',
-							newest_on_top: true,
-							allow_dismiss: true,
-							template: '<div data-notify="container" class="col-xs-6 col-sm-3 alert alert-{0}" role="alert">' +
-							'<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
-							'<span data-notify="icon"></span> ' +
-							'<span data-notify="title">{1}</span> ' +
-							'<span data-notify="message">{2}</span>' +
-							'</div>' ,
+						//settings
+						type: 'success',
+						newest_on_top: true,
+						allow_dismiss: true,
+						template: '<div data-notify="container" class="col-xs-6 col-sm-3 alert alert-{0}" role="alert">' +
+						'<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+						'<span data-notify="icon"></span> ' +
+						'<span data-notify="title">{1}</span> ' +
+						'<span data-notify="message">{2}</span>' +
+						'</div>' ,
 						});
 					}
 					return cb();
 				});
 			};
 
-		this.throw_error = (cmd,params) => {
-			var title;
-			switch(cmd){
-				case "allocate":
-				title = "Provider not found"
-				break;
-				case "create":
-				title = "Error creation instance"
-				break;
-				case "stop":
-				title = "Error kill instance"
-				break;
-				case "remove":
-				title = "Error remove instance"
-				break;						
-				default:
-				title = "Error Unknown command"
-			}
-			$.notify({
-												// options
-												icon: 'glyphicon glyphicon-remove-sign',
-												title: title+"<br>",
-												message: params,
-											},{
-												//settings
-												type: 'danger',
-												newest_on_top: true,
-												allow_dismiss: true,
-												template: '<div data-notify="container" class="col-xs-6 col-sm-3 alert alert-{0}" role="alert">' +
-												'<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
-												'<span data-notify="icon"></span> ' +
-												'<span data-notify="title">{1}</span> ' +
-												'<span data-notify="message">{2}</span>' +
-												'</div>' ,
-											});
-		};
+			this.throw_error = (cmd,params) => {
+				var title;
+				switch(cmd){
+					case "allocate":
+					title = "Provider not found"
+					break;
+					case "create":
+					title = "Error creation instance"
+					break;
+					case "stop":
+					title = "Error kill instance"
+					break;
+					case "remove":
+					title = "Error remove instance"
+					break;						
+					default:
+					title = "Error Unknown command"
+				}
+				$.notify({
+					// options
+					icon: 'glyphicon glyphicon-remove-sign',
+					title: title+"<br>",
+					message: params,
+					},{
+					//settings
+					type: 'danger',
+					newest_on_top: true,
+					allow_dismiss: true,
+					template: '<div data-notify="container" class="col-xs-6 col-sm-3 alert alert-{0}" role="alert">' +
+					'<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+					'<span data-notify="icon"></span> ' +
+					'<span data-notify="title">{1}</span> ' +
+					'<span data-notify="message">{2}</span>' +
+					'</div>' ,
+				});
+			};
 
-		this.throw_success = (cmd,param) => {
+			this.throw_success = (cmd,param) => {
 				var title;
 				var msg=param;
 				switch(cmd){
@@ -185,167 +185,168 @@ angular.module('iaas-collaboratif').directive('user', function () {
 					title = "Unknown command"
 				}
 				$.notify({
-				            // options
-				            icon: 'glyphicon glyphicon-ok-sign',
-				            title: title,
-				            message: msg,
-				        },{
-				            //settings
-				            type: 'success',
-				            newest_on_top: true,
-				            allow_dismiss: true,
-				            template: '<div data-notify="container" class="col-xs-6 col-sm-3 alert alert-{0}" role="alert">' +
-				            '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
-				            '<span data-notify="icon"></span> ' +
-				            '<span data-notify="title">{1}</span> ' +
-				            '<span data-notify="message">{2}</span>' +
-				            '</div>' ,
-				        });
+					// options
+					icon: 'glyphicon glyphicon-ok-sign',
+					title: title,
+					message: msg,
+					},{
+					//settings
+					type: 'success',
+					newest_on_top: true,
+					allow_dismiss: true,
+					template: '<div data-notify="container" class="col-xs-6 col-sm-3 alert alert-{0}" role="alert">' +
+					'<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+					'<span data-notify="icon"></span> ' +
+					'<span data-notify="title">{1}</span> ' +
+					'<span data-notify="message">{2}</span>' +
+					'</div>' ,
+				});
 			};
 
-
-		this.getInfoFromRessource = (ressource_id, cb) => {
-			Meteor.call('getInfoFromRessource',ressource_id, function (err, response) {
-				if(err){
-					self.throw_error('allocate','The provider does not exist anymore.')
-				}
-				return cb(err, response);
-			});
-		}
-
-		this.allocate = (machine) => {
-			var self = this;
-			this.currentUser.getSubscriber().allocate(machine, function (err, new_machine) {
-				if (err) return console.error("Failed to reallocate, allocation failed", err);
-				self.getInfoFromRessource(new_machine.machine.ressource_id, function (err, isItAvailable) {
-					if(err) return console.error("An error occured while reallocating the machine", err);
-					if(isItAvailable.usable){
-						machine=Machines.findOne({_id:machine._id});
-						self.action_user('create',machine.machinetype+' 1 '+machine.machinename+' '+machine.ram+'G '+machine.cpunumber+' '+isItAvailable.ram+'G '+isItAvailable.storage+'G',function(){});
+			this.getInfoFromRessource = (ressource_id, cb) => {
+				Meteor.call('getInfoFromRessource',ressource_id, function (err, response) {
+					if(err){
+						self.throw_error('allocate','The provider does not exist anymore.')
 					}
-				});	
-			})
-		}
+					return cb(err, response);
+				});
+			}
 
-		this.startMachine = (machine) => {
-			this.save();
-			var self = this;
-			this.getInfoFromRessource(machine.ressource_id, function (err, resourceInfo) {
-				if(err || resourceInfo.err){
-					// reallocating the ressource
-					self.throw_success('reallocate','The machine that you are trying to start could be removed or deplaced since the current provider is not accessible.')
-					machine.machinename=self.currentUser.username;
+			this.allocate = (machine) => {
+				var self = this;
+				this.currentUser.getSubscriber().allocate(machine, function (err, new_machine) {
+					if (err) return console.error("Failed to reallocate, allocation failed", err);
+					self.getInfoFromRessource(new_machine.machine.ressource_id, function (err, isItAvailable) {
+						if(err) return console.error("An error occured while reallocating the machine", err);
+						if(isItAvailable.usable){
+							machine=Machines.findOne({_id:machine._id});
+							self.action_user('create',machine.machinetype+' 1 '+machine.machinename+' '+machine.ram+'G '+machine.cpunumber+' '+isItAvailable.ram+'G '+isItAvailable.storage+'G',function(){});
+						}
+					});	
+				})
+			}
 
-					Machines.remove({_id: machine._id},(error) => {
-						if (error) this.throw_error('remove','Unable to remove machine')
-					});
+			this.startMachine = (machine) => {
+				this.save();
+				var self = this;
+				this.getInfoFromRessource(machine.ressource_id, function (err, resourceInfo) {
+					if(err || resourceInfo.err){
+						// reallocating the ressource
+						self.throw_success('reallocate','The machine that you are trying to start could be removed or deplaced since the current provider is not accessible.')
+						machine.machinename=self.currentUser.username;
 
-					self.allocate(machine);
-					return;
-				}
-				if(! resourceInfo.usable)
-				{
-					// reallocating the ressource
-					self.throw_success('reallocate','The machine that you are trying to start could be removed or deplaced since the current provider is not accessible.')
-					machine.machinename=self.currentUser.username;
-					self.currentUser.getSubscriber().desallocate(machine, function (err, resp) {
-						if (err) return console.error("Failed to reallocate, desallocation failed", err);
+						Machines.remove({_id: machine._id},(error) => {
+							if (error) this.throw_error('remove','Unable to remove machine')
+						});
+
 						self.allocate(machine);
-					})
-				}
-				else{
-					self.action_user('create',machine.machinetype+' 1 '+machine.machinename+' '+machine.ram+'G '+machine.cpunumber+' '+resourceInfo.ram+'G '+resourceInfo.storage+'G',function(){});
-				}
-			});
-		};
-
-
-		this.stopMachine = (machine) => {
-			machine.state='down';
-			Machines.update({_id: machine._id}, {$set:{state:machine.state}}, (error) => {
-				if (error) this.throw_error('stop','Unable to stop machine');
-				else this.action_user('stop',machine);
-			});
-		};
-
-		this.deleteMachine = (machine) => {
-			this.save();
-			this.currentUser.getSubscriber().desallocate(machine, function(){});
-			// notif done in subscriber.js
-			// --> should be done on the client side and not in the model side reserved for databases and modeling
-		};		
-
-		this.generate = (machine) => {
-
-			makeTextFile = function (text) {
-				var data = new Blob([text], {type: 'text/plain'});
-				if (textFile !== null) {
-					window.URL.revokeObjectURL(textFile);
-				}
-				var textFile = window.URL.createObjectURL(data);
-				return textFile;
+						return;
+					}
+					if(! resourceInfo.usable)
+					{
+						// reallocating the ressource
+						self.throw_success('reallocate','The machine that you are trying to start could be removed or deplaced since the current provider is not accessible.')
+						machine.machinename=self.currentUser.username;
+						self.currentUser.getSubscriber().desallocate(machine, function (err, resp) {
+							if (err) return console.error("Failed to reallocate, desallocation failed", err);
+							self.allocate(machine);
+						})
+					}
+					else{
+						self.action_user('create',machine.machinetype+' 1 '+machine.machinename+' '+machine.ram+'G '+machine.cpunumber+' '+resourceInfo.ram+'G '+resourceInfo.storage+'G',function(){});
+					}
+				});
 			};
 
-			function downloadURI(uri, name) {
-				var link = document.createElement("a");
-				link.download = name;
-				link.href = uri;
-				document.body.appendChild(link);
-				link.click();
-				document.body.removeChild(link);
-			}
 
-			var ssh_string='# Host is an alias , Hostname is the name of the user instance\n';
-			ssh_string+='# We presume that you private key is  ~/.ssh/id_rsa\n';
-			ssh_string+='# To ssh your instance : type the following command\n';
-			ssh_string+='# ssh -F iaas-'+machine.machinename+'.config '+machine.machinename+'\n';
-			ssh_string+='Host '+machine.machinename+'\n';
-			ssh_string+='\tHostname '+machine.machinename+'\n';
-			ssh_string+='\tStrictHostKeyChecking no\n';
-			ssh_string+='\tProxyCommand  ssh -i ~/.ssh/id_rsa iaas-client@'+machine.dns+' netcat %h %p\n';
-			ssh_string+='\tUser root\n';
-
-			downloadURI(makeTextFile(ssh_string),'iaas-'+machine.machinename+'.config');
-		};
-
-		this.generate_all = () => {
-
-			makeTextFile = function (text) {
-				var data = new Blob([text], {type: 'text/plain'});
-				if (textFile !== null) {
-					window.URL.revokeObjectURL(textFile);
-				}
-				var textFile = window.URL.createObjectURL(data);
-				return textFile;
+			this.stopMachine = (machine,cb) => {
+				machine.state='down';
+				Machines.update({_id: machine._id}, {$set:{state:machine.state}}, (error) => {
+					if (error) this.throw_error('stop','Unable to stop machine');
+					else this.action_user('stop',machine,function(){return cb();});
+				});
 			};
 
-			function downloadURI(uri, name) {
-				var link = document.createElement("a");
-				link.download = name;
-				link.href = uri;
-				document.body.appendChild(link);
-				link.click();
-				document.body.removeChild(link);
-			}
+			this.deleteMachine = (machine) => {
+				var self = this;
+				this.stopMachine(machine,function(){
+					self.currentUser.getSubscriber().desallocate(machine, function(){});
+				});
+				// notif done in subscriber.js
+				// --> should be done on the client side and not in the model side reserved for databases and modeling
+			};		
 
-			var ssh_string="";
-			ssh_string+='# Host is an alias , Hostname is the name of the user instance\n';
-			ssh_string+='# We presume that you private key is  ~/.ssh/id_rsa\n';
-			ssh_string+='# To ssh your instance :\n';
-			ssh_string+='# ssh -F <config_file> <instance_name>\n';
+			this.generate = (machine) => {
 
-			for(i=0;i<this.machines.length;i++){
-				var machine = this.machines[i];
-				ssh_string+='#Instance '+machine.machinename+'\n';
+				makeTextFile = function (text) {
+					var data = new Blob([text], {type: 'text/plain'});
+					if (textFile !== null) {
+						window.URL.revokeObjectURL(textFile);
+					}
+					var textFile = window.URL.createObjectURL(data);
+					return textFile;
+				};
+
+				function downloadURI(uri, name) {
+					var link = document.createElement("a");
+					link.download = name;
+					link.href = uri;
+					document.body.appendChild(link);
+					link.click();
+					document.body.removeChild(link);
+				}
+
+				var ssh_string='# Host is an alias , Hostname is the name of the user instance\n';
+				ssh_string+='# We presume that you private key is  ~/.ssh/id_rsa\n';
+				ssh_string+='# To ssh your instance : type the following command\n';
+				ssh_string+='# ssh -F iaas-'+machine.machinename+'.config '+machine.machinename+'\n';
 				ssh_string+='Host '+machine.machinename+'\n';
 				ssh_string+='\tHostname '+machine.machinename+'\n';
 				ssh_string+='\tStrictHostKeyChecking no\n';
 				ssh_string+='\tProxyCommand  ssh -i ~/.ssh/id_rsa iaas-client@'+machine.dns+' netcat %h %p\n';
 				ssh_string+='\tUser root\n';
-			}
 
-			downloadURI(makeTextFile(ssh_string),'iaas-'+this.currentUser.username+'.config');
-		};
+				downloadURI(makeTextFile(ssh_string),'iaas-'+machine.machinename+'.config');
+			};
+
+			this.generate_all = () => {
+
+				makeTextFile = function (text) {
+					var data = new Blob([text], {type: 'text/plain'});
+					if (textFile !== null) {
+						window.URL.revokeObjectURL(textFile);
+					}
+					var textFile = window.URL.createObjectURL(data);
+					return textFile;
+				};
+
+				function downloadURI(uri, name) {
+					var link = document.createElement("a");
+					link.download = name;
+					link.href = uri;
+					document.body.appendChild(link);
+					link.click();
+					document.body.removeChild(link);
+				}
+
+				var ssh_string="";
+				ssh_string+='# Host is an alias , Hostname is the name of the user instance\n';
+				ssh_string+='# We presume that you private key is  ~/.ssh/id_rsa\n';
+				ssh_string+='# To ssh your instance :\n';
+				ssh_string+='# ssh -F <config_file> <instance_name>\n';
+
+				for(i=0;i<this.machines.length;i++){
+					var machine = this.machines[i];
+					ssh_string+='#Instance '+machine.machinename+'\n';
+					ssh_string+='Host '+machine.machinename+'\n';
+					ssh_string+='\tHostname '+machine.machinename+'\n';
+					ssh_string+='\tStrictHostKeyChecking no\n';
+					ssh_string+='\tProxyCommand  ssh -i ~/.ssh/id_rsa iaas-client@'+machine.dns+' netcat %h %p\n';
+					ssh_string+='\tUser root\n';
+				}
+
+				downloadURI(makeTextFile(ssh_string),'iaas-'+this.currentUser.username+'.config');
+			};
+		}
 	}
-}
 });
