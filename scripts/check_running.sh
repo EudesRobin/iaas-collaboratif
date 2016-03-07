e1ddbc7d0b302eefca551a07efe8be76dda41a64
Malek Mammar <malek.mammar@voxity.fr>
2016-03-07 18:17:56 +0100

    adding settings option to check_running.sh

--- scripts/check_running.sh
+++ scripts/check_running.sh
@@ -4,4 +4,4 @@
 
 if ! ps aux | grep -v grep | grep ${HOME}/iaas-collaboratif/meteor-frontend/.meteor/local/build/main.js > /dev/null ;then
 cd ${HOME}/iaas-collaboratif/meteor-frontend/
-screen -S meteor -d -m meteor run -p 3000 --production;fi
+screen -S meteor -d -m meteor run -p 3000 --settings settings.json --production;fi
