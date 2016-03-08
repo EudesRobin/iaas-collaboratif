ea61b44da852230628a4adc8e9e3df834c8a1714
Malek Mammar <malek.mammar@voxity.fr>
2016-03-08 11:45:46 +0100

    updating publish script to fanout

--- docker/coordinator/publisher/publish.js
+++ docker/coordinator/publisher/publish.js
@@ -1,82 +1,23 @@
-// Example of using a TLS/SSL connection. Note that the server must be
-// configured to accept SSL connections; see, for example,
-// http://www.rabbitmq.com/ssl.html.
-//
-// When trying this out, I followed the RabbitMQ SSL guide above,
-// almost verbatim. I set the CN of the server certificate to
-// 'localhost' rather than $(hostname) (since on my MBP hostname ends
-// up being "<blah>.local", which is just weird). My client
-// certificates etc., are in `../etc/client/`. My testca certificate
-// is in `../etc/testca` and server certs etc., in `../etc/server`,
-// and I've made a `rabbitmq.config` file, with which I start
-// RabbitMQ:
-//
-//     RABBITMQ_CONFIG_FILE=`pwd`/../etc/server/rabbitmq \
-//       /usr/local/sbin/rabbitmq-server &
-//
-// A way to check RabbitMQ's running with SSL OK is to use
-//
-//     openssl s_client -connect localhost:5671
 
-var amqp = require('amqplib');
+
+var amqp = require('amqplib/callback_api');
 var fs = require('fs');
 var settings = require('./settings.json')
 
-// Assemble the SSL options; for verification we need at least
-// * a certificate to present to the server ('cert', in PEM format)
-// * the private key for the certificate ('key', in PEM format)
-// * (possibly) a passphrase for the private key
-//
-// The first two may be replaced with a PKCS12 file ('pfx', in pkcs12
-// format)
-
-// We will also want to list the CA certificates that we will trust,
-// since we're using a self-signed certificate. It is NOT recommended
-// to use `rejectUnauthorized: false`.
-
-// Options for full client and server verification:
-// var opts = {
-//   url: 'amqps://localhost',
-//   cert: fs.readFileSync('./client/cert.pem'),
-//   key: fs.readFileSync('./client/key.pem'),
-//   // cert and key or
-//   // pfx: fs.readFileSync('../etc/client/keycert.p12'),
-//   passphrase: 'MySecretPassword',
-//   ca: [fs.readFileSync('./testca/cacert.pem')]
-// };
 
 var opts = {
   url: "amqp://"+settings.rabbitmq.user+":"+settings.rabbitmq.password+"@"+settings.rabbitmq.host+":"+settings.rabbitmq.port,
 };
 
-// var opts = {
-//   cert: fs.readFileSync('./provider/cert.pem'),
-//   key: fs.readFileSync('./provider/key.pem'),
-//   // cert and key or
-//   // pfx: fs.readFileSync('../etc/client/keycert.p12'),
-//   passphrase: 'MySecretPassword',
-//   ca: [fs.readFileSync('./testca/cacert.pem')]
-// };
-
-// Options for just confidentiality. This requires RabbitMQ's SSL
-// configuration to include the items
-//
-//     {verify, verify_none},
-//     {fail_if_no_peer_cert,false}
-//
-// var opts = {  ca: [fs.readFileSync('../etc/testca/cacert.pem')] };
-
-// Option to use the SSL client certificate for authentication
-// opts.credentials = amqp.credentials.external();
-
-var open = amqp.connect(opts.url, opts);
-
-open.then(function(conn) {
-  process.on('SIGINT', conn.close.bind(conn));
-  return conn.createChannel().then(function(ch) {
-    console.log("sending file", process.argv[2])
+amqp.connect(opts.url, function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var ex = 'coordinators';
     var file = fs.readFileSync(process.argv[2])
-    ch.sendToQueue('coordinators', new Buffer(file));
-    conn.close.bind(conn);
+    ch.assertExchange(ex, 'fanout', {durable: false});
+
+    ch.publish(ex, '', file);
+    console.log(" [x] Sent %s", process.argv[2]);
   });
-}).then(null, console.warn);
+
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});
