6ba0b20899069aebcb0c4cf5368276241a094b51
EudesRobin <eudes.robin@gmail.com>
2016-03-08 16:43:34 +0100

    [REU] up provider

--- meteor-frontend/server/rabbitmq.js
+++ meteor-frontend/server/rabbitmq.js
@@ -1,7 +1,7 @@
 Meteor.startup(function () {
 	var amqp = Meteor.npmRequire('amqplib/callback_api')
 	var channel = null
-    
+
 	var url = "amqp://"+Meteor.settings.rabbitmq.user+":"+Meteor.settings.rabbitmq.password+"@"+Meteor.settings.rabbitmq.host+":"+Meteor.settings.rabbitmq.port;
 
     var variables = {
@@ -11,7 +11,7 @@ Meteor.startup(function () {
     }
     // connecting to amqp
     amqp.connect(url, function(err, conn) {
-       if (err != null) return console.error("AMQP - Failed to create a connection", err);
+     if (err != null) return console.error("AMQP - Failed to create a connection", err);
         // creating a channel
         conn.createChannel(function (err, ch) {
           if (err) return console.error("AMQP - Failed to create a channel", err);
@@ -20,7 +20,7 @@ Meteor.startup(function () {
             ch.assertExchange(variables.exchange, variables.type, {durable: false});
             // creating a random queue
             ch.assertQueue(variables.queue, {exclusive: true}, function(err, ok){
-             if (err) return console.error("AMQP - Failed to create a queue", err)
+               if (err) return console.error("AMQP - Failed to create a queue", err)
                 // attaching a consumer to the queue
             ch.consume(ok.queue, handler, {noAck: true}, function (err) {
                 if (err) return console.error("AMQP - Failed to attach a consumer", err)
@@ -50,24 +50,10 @@ Meteor.startup(function () {
 });
 
 var doWork = Meteor.bindEnvironment(function (instances) {
-   instances.forEach(function (instance) {
-    var instance_name = instance.Name.split('/')[1];
-    var name = instance.Name.split("-");
-    var username = name[0].split('/')[1];
-    name.splice(0,1);
-    name.splice(name.length-1,1);
-    name = name.join("-");
-    var dns = name;
-    var ressource = Ressources.find({dns: dns}).fetch();
-    if (ressource.length != 1) return console.error("Either more than one and no ressoure was found !")
+ instances.forEach(function (instance) {
 
-                // updating the ressource state to USABLE
-            Ressources.update({ressource: ressource._id}, {
-                $set:{usable: true}
-            })
-
-            if (instance.Name === "/coordinator")
-            {
+    if (instance.Name === "/coordinator")
+    {
     			// do something
     		} 
     		else if (instance.Name === "/cadvisor")
@@ -76,6 +62,21 @@ var doWork = Meteor.bindEnvironment(function (instances) {
     		}
     		else
     		{
+                var instance_name = instance.Name.split('/')[1];
+                var name = instance.Name.split("-");
+                var username = name[0].split('/')[1];
+                name.splice(0,1);
+                name.splice(name.length-1,1);
+                name = name.join("-");
+                var dns = name;
+                var ressource = Ressources.find({dns: dns}).fetch();
+                if (ressource.length != 1) return console.error("Either more than one and no ressoure was found !")
+
+                // updating the ressource state to USABLE
+            Ressources.update({ressource: ressource._id}, {
+                $set:{usable: true}
+            })
+
 
     			// updating machine infos
     			Machines.update({machinename: instance_name}, {
