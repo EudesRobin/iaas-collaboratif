a8615d6d96da8795c8b78a1d4851d7440cc050d7
Malek Mammar <malek.mammar@voxity.fr>
2016-03-09 16:20:01 +0100

    adding rabbitmq reconnection processing

--- docker/coordinator/publisher/publish.js
+++ docker/coordinator/publisher/publish.js
@@ -4,33 +4,91 @@ var amqp = require('amqplib/callback_api');
 var later = require('later');
 var fs = require('fs');
 var settings = require('./settings.json')
-var channel = null;
+var amqpConn = null;
 var ex = 'coordinators';
 
 var opts = {
   url: "amqp://"+settings.rabbitmq.user+":"+settings.rabbitmq.password+"@"+settings.rabbitmq.host+":"+settings.rabbitmq.port,
 };
 
-amqp.connect(opts.url, function(err, conn) {
-  if (err) console.error("Failed to connect to the broker", err)
-  conn.createChannel(function(err, ch) {
-    if (err) console.error("Failed to create a channel", err)
-    channel = ch;
-    ch.assertExchange(ex, 'fanout', {durable: false});
 
-    // console.log(" [x] Sent %s", process.argv[2]);
-    // ch.publish(ex, '', file);
-  	// setTimeout(function() { conn.close(); process.exit(0) }, 500);
+function start() {
+  amqp.connect(opts.url + "?heartbeat=60", function(err, conn) {
+    if (err) {
+      console.error("[AMQP]", err.message);
+      return setTimeout(start, 1000);
+    }
+    conn.on("error", function(err) {
+      if (err.message !== "Connection closing") {
+        console.error("[AMQP] conn error", err.message);
+      }
+    });
+    conn.on("close", function() {
+      console.error("[AMQP] reconnecting");
+      return setTimeout(start, 1000);
+    });
+
+    console.log("[AMQP] connected");
+    amqpConn = conn;
+
+    whenConnected();
   });
+}
 
-});
+function whenConnected() {
+  startPublisher();
+}
+
+var pubChannel = null;
+var offlinePubQueue = [];
+function startPublisher() {
+  amqpConn.createConfirmChannel(function(err, ch) {
+    if (closeOnErr(err)) return;
+    ch.on("error", function(err) {
+      console.error("[AMQP] channel error", err.message);
+    });
+    ch.on("close", function() {
+      console.log("[AMQP] channel closed");
+    });
+    ch.assertExchange(ex, 'fanout', {durable: false});
+    pubChannel = ch;
+    while (true) {
+      var m = offlinePubQueue.shift();
+      if (!m) break;
+      publish(m[0], m[1], m[2]);
+    }
+  });
+}
+
+// method to publish a message, will queue messages internally if the connection is down and resend later
+function publish(exchange, routingKey, content) {
+  try {
+    pubChannel.publish(exchange, routingKey, content, { persistent: false },
+                       function(err, ok) {
+                         if (err) {
+                           console.error("[AMQP] publish", err);
+                           offlinePubQueue.push([exchange, routingKey, content]);
+                           pubChannel.connection.close();
+                         }
+                       });
+  } catch (e) {
+    console.error("[AMQP] publish", e.message);
+    offlinePubQueue.push([exchange, routingKey, content]);
+  }
+}
+
+function closeOnErr(err) {
+  if (!err) return false;
+  console.error("[AMQP] error", err);
+  amqpConn.close();
+  return true;
+}
+
+start();
 
 var sched = later.parse.text('every 1 seconds');
 later.setInterval(function () {
-	if (channel) 
-	{
-		var file = fs.readFileSync(process.argv[2])
-		channel.publish(ex, '', file);
-		console.log(" [x] Sent %s", process.argv[2]);
-	}
+	var file = fs.readFileSync(process.argv[2])
+	publish(ex, '', file);
+	console.log(" [x] Sent %s", process.argv[2]);
 }, sched)
