e3e48431fdda22f6632bfc610979cc7d25d97e52
AlanDamotte <Alan.damotte@gmail.com>
2016-03-14 14:08:38 +0100

    [ADA] Modify coordinatorStop: no longer remove containers

--- docker/coordinatorStop.sh
+++ docker/coordinatorStop.sh
@@ -9,14 +9,14 @@
 #========================================================
 # Stopping shinken
 docker stop cadvisor
-docker rm -f cadvisor
+#docker rm -f cadvisor
 #========================================================
 
 #========================================================
 # Stopping coordinator
 coordinatorName=$(docker ps -a| grep "coordinator*"|awk '{print $(NF)}')
 docker stop $coordinatorName
-docker rm $coordinatorName
+#docker rm $coordinatorName
 #========================================================
 
 crontab -l | grep -v '* * * * * /home/iaas/watchdog.sh && sleep 30 && /home/iaas/watchdog.sh' | sudo crontab -
